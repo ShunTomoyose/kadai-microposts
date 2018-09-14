66e1ad49cdcb9b2fa44dc75e27e7307a7646fc97
shun.tomoyose <shun.tomoyose@g.softbank.co.jp>
2018-09-14 05:30:48 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  private
+  
+  def require_user_logged_in
+    unledd logged_in?
+      redirect_to login_url
+    end
+  end
 end
