2a4a6a1a8363045f07d26ec8f883e5421220a8ca
shun.tomoyose <shun.tomoyose@g.softbank.co.jp>
2018-09-14 09:43:25 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+  
+  
   def index
     @users = User.all.page(params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order('created_at desc').page(params[:page])
+    counts(@user)
   end
 
   def new
